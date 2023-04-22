322a545aab441a3c9fab52adf6bdfbe6d04341e2
Shaked Lanciano <88950077+MrsHorrid@users.noreply.github.com>
2023-04-22 19:38:34 +0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+    import "../app.css";
+  </script>
+<!--   
+  <nav class="flex justify justify-between p-4 px-8 items-center bg-white m-10 rounded-[2rem]">
+    <div>
+      <img src="https://cdn.discordapp.com/attachments/1063591220028383252/1099083245691355136/xx.png" alt="" class="w-12">
+    </div>
+    <div class="flex gap-4 font-semibold ">
+      <a href="/" class="hover:text-[#7751ff] ease-in-out duration-300">Home</a>
+      <a href="/about" class="hover:text-[#7751ff] ease-in-out duration-300">About</a>
+      <a href="/contact" class="hover:text-[#7751ff] ease-in-out duration-300">Contact</a>
+    </div>
+
+  </nav> -->
+  <main>
+    <slot />
+  </main>
+
+  <style>
+    :global(html)
+    {
+      background-color: #4d1cff;
+    }
+  </style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+	import Card from "../lib/components/Card.svelte";
+
+
+</script>
+
+
+<div class="mb-8 text-white text-center p-6 m-6 px-[10rem] mt-[5rem]">
+<h1 class="text-6xl font-bold">We Are Validify!</h1>
+<p class="p-6 px-10 m-6 text-2xl">with our blockchain you can secure anything from metrics to preformance and even project proof of work</p>
+
+<a class="bg-black py-4 px-7 rounded-xl hover:bg-white hover:text-black ease-in-out duration-300 font-bold" href="/about">About Us</a>
+
+</div>
+
+<div class="p-8">
+    <img src="https://cdn.discordapp.com/attachments/1063591220028383252/1099092004903862403/pitch.png" alt="" class="w-[80rem] m-auto border-[#826cff] border-[1rem] rounded-[2rem] shadow-lg">
+
+</div>
+
+<div class="bg-white p-8">
+    <div class="flex items-center justify-between p-8 gap-5">
+        <div class="p-8">
+            <h1 class="text-5xl font-bold">Track Metrics on all of your users</h1>
+            <p class="mt-4">view advanced statistics with ease no matter where you are it will always be accesible</p>
+        </div>
+        <div>
+            <img src="https://cdn.discordapp.com/attachments/1063591220028383252/1099352324356456458/Frame_1.png" alt="" class="w-[45rem] hover:w-[48rem] ease-in-out duration-300">
+        </div>
+    </div>
+
+</div>
+
+<div class="p-8">
+    <div class="grid-cols-2 grid gap-4 p-8 px-[5rem]">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+    </div>
+</div>
+
+<div class="p-8 bg-white">
+    <div class="flex items-center p-8 justify-between gap-4">
+        <div>
+            <img src="https://cdn.discordapp.com/attachments/1084571551346737152/1099358775607046204/Frame_2.png" alt="" class="w-[45rem] hover:w-[48rem] ease-in-out duration-300">
+        </div>
+        <div class="p-6">
+            <h1 class="text-5xl font-bold">Advanced Dahsboard</h1>
+            <p class="mt-4">track advanced statistics on all of your projects</p>
+        </div>
+    </div>
+</div>
+
